985471ec36ce6a57a61dda4c755e11211faa0343
M.-Leander Reimer <mario-leander.reimer@qaware.de>
2022-08-11 00:46:57 +0200

    Added Prometheus /metrics.

--- main.go
+++ main.go
@@ -4,10 +4,23 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/penglongli/gin-metrics/ginmetrics"
 )
 
 func main() {
 	engine := gin.Default()
+
+	// get global Monitor object
+	m := ginmetrics.GetMonitor()
+	// +optional set metric path, default /debug/metrics
+	m.SetMetricPath("/metrics")
+	// +optional set slow time, default 5s
+	m.SetSlowTime(10)
+	// +optional set request duration, default {0.1, 0.3, 1.2, 5, 10}
+	// used to p95, p99
+	m.SetDuration([]float64{0.1, 0.3, 1.2, 5, 10})
+	// set middleware for gin
+	m.Use(engine)
 
 	// configuration for static files and templates
 	engine.LoadHTMLFiles("templates/index.html")
